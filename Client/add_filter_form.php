5c6cd5cf68d7e8b6891f8d7fc31d42f2ac84db94
Sivaramakrishnan Satyamangalam Ramanathan <Shivram@Sivaramakrishnans-MacBook-Pro.local>
2016-03-11 15:51:50 -0800

    Added folders

---
+++ Client/add_filter_form.php
@@ -0,0 +1,110 @@
+<!--
+
+#
+# Copyright (C) 2016 University of Southern California.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+          <title>SENSS</title>
+          <link rel="stylesheet" href="css/bootstrap.min.css">
+          <script src="css/jquery.min.js"></script>
+          <script src="css/bootstrap.min.js"></script>
+	  <style>
+                .panel { width:300px; margin:auto; padding: 30px;}
+                .panel-offset-senss { margin:auto;}
+   	  </style>
+</head>
+
+<body>
+        <nav class="navbar navbar-inverse navbar-static-top">
+                <div class="container-fluid">
+                        <div class="navbar-header">
+                                <a class="navbar-brand" href="index.php">SENSS-ISP</a>
+                        </div>
+                        <div>
+                                <ul class="nav navbar-nav">
+                                        <li><a href="add_filter_form.php">Add Filter</a></li>
+                                        <li><a href="get_flows_form.php">Flow Stats</a></li>
+                                        <li><a href="get_routes_form.php">Route Stats</a></li>
+					<li><a href="direct_floods_form.php">Direct Floods</a></li>
+                                        <li><a href="pic.php">View</a></li>
+                                </ul>
+                        </div>
+                </div>
+        </nav>
+
+
+	<div class="panel panel-default">
+        	<div class="panel-offset-senss">
+                	<form name="add_filter_form" id="add_filter_form" action="add_filter.php" method="post">
+                        	<div class="form-group">
+                                	<input type="text" style="width:200px;" class="form-control" name="nw_src" id="nw_src" maxlength="50"  placeholder="Source IP" />
+                        	</div>
+                        	<div class="form-group">
+                                	<input type="text" style="width:200px;" class="form-control" name="nw_dst" id="nw_dst" maxlength="50"  placeholder="Destination IP" />
+                        	</div>
+                        	<div class="form-group">
+                            	  	<input type="text" style="width:200px;" class="form-control" name="in_port" id="in_port" maxlength="50"  placeholder="In Port" />
+                        	</div>
+                        	<div class="form-group">
+                                	<input type="text" style="width:200px;" class="form-control" name="eth_dst" id="eth_dst" maxlength="50"  placeholder="Destination MAC" />
+                        	</div>
+                        	<div class="form-group">
+                               		<input type="text" style="width:200px;" class="form-control" name="eth_src" id="eth_src" maxlength="50"  placeholder="Source MAC" />
+                        	</div>
+                        	<div class="form-group">
+                                	<input type="text" style="width:200px;" class="form-control" name="dl_vlan" id="dl_vlan" maxlength="50"  placeholder="VLAN ID" />
+                        	</div>
+                        	<div class="form-group">
+                                	<input type="text" style="width:200px;" class="form-control" name="tcp_src" id="tcp_src" maxlength="50"  placeholder="TCP Source Port" />
+                        	</div>
+                        	<div class="form-group">
+                                	<input type="text" style="width:200px;" class="form-control" name="tcp_dst" id="tcp_dst" maxlength="50"  placeholder="TCP Destination Port" />
+                        	</div>
+                        	<div class="form-group">
+                                	<input type="text" style="width:200px;" class="form-control" name="udp_src" id="udp_src" maxlength="50"  placeholder="UDP Source Port" />
+                        	</div>
+                        	<div class="form-group">
+                                	<input type="text" style="width:200px;" class="form-control" name="udp_dst" id="udp_dst" maxlength="50"  placeholder="UDP Destination Port" />
+                        	</div>
+			        <?php
+        	        		$url='http://192.168.0.96:8080/stats/switches';
+                			$ch=curl_init($url);
+               	 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                			$result = curl_exec($ch);
+                			curl_close($ch);
+                			$json_output = json_decode($result,true);
+                			$switch_count=0;
+                			echo '<label>Select ISP:</label>';
+                			echo '<select class="form-control" name="dpid">';
+                			foreach($json_output as $item){
+                        			$switch_dpid=$item;
+                        			$switch_count=$switch_count+1;
+                        			$command='python /var/www/html/scripts/dpid_to_city.py '.$switch_dpid.' 2>&1';
+                        			$switch_name=trim(shell_exec($command));
+                        			echo '<option value="'.$switch_dpid.'">'.$switch_name.'</option>';
+                			}
+                			echo '</select>';
+		        	?>
+				<br />
+                        	<input type="submit" class="btn" name="formSubmit" value="Submit"/>
+                	</form>
+        	</div>
+	</div>
+</body>
+</html>
---
+++ Server/remove_switch_form.php
@@ -0,0 +1,68 @@
+<!--
+
+#
+# Copyright (C) 2016 University of Southern California.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	  <title>SENSS</title>
+	  <link rel="stylesheet" href="css/bootstrap.min.css">
+  	  <script src="css/jquery.min.js"></script>
+  	  <script src="css/bootstrap.min.js"></script>
+          <style>
+                .panel { width:300px; margin:auto; padding: 30px;}
+                .panel-offset-senss { margin:auto;}
+          </style> 
+</head>
+
+<body>
+        <nav class="navbar navbar-inverse navbar-static-top">
+                <div class="container-fluid">
+                        <div class="navbar-header">
+                                <a class="navbar-brand" href="index.php">SENSS-ISP</a>
+                        </div>
+                        <div>
+                                <ul class="nav navbar-nav">
+                                        <li><a href="add_switch_form.php">Add Switch</a></li>
+                                        <li><a href="get_switch.php">List Switches</a></li>
+                                        <li><a href="remove_switch_form.php">Remove Switch</a></li>
+                                        <li><a href="logs.php">Logs</a></li>
+                                        <li><a href="add_customer_form.php">Add Customer</a></li>
+                                        <li><a href="view_customer.php">View Customer</a></li>
+                                        <li><a href="http://localhost/index.php">Log Out</a></li>
+                                </ul>
+                        </div>
+                </div>
+        </nav>
+	<div class="panel panel-default panel-offset-senss">
+	<form action="remove_switch.php" method="post">
+			<div class="form-group">
+                                <input type="text" style="width:200px;" class="form-control" name="switch_name" id="switch_name" maxlength="50"  placeholder="Name of Switch" />
+                        </div>
+			<div class="form-group">
+                                <input type="text" style="width:200px;" class="form-control" name="switch_username" id="switch_username" maxlength="50"  placeholder="Switch Username" />
+                        </div>
+			<div class="form-group">
+                                <input type="password" style="width:200px;" class="form-control" name="switch_password" id="switch_password" maxlength="50"  placeholder="Switch Password" />
+                        </div>
+			<input type="submit" name="formSubmit" class="btn" value="Submit" />
+	</form>
+	</div>
+
+</body>
+</html>
