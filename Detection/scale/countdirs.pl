63258acc3cf58b5210e52f8900e363fad87333aa
Steel ISI <ahanamsa@usc.edu>
2017-01-19 10:51:50 -0800

    Process create and wait for POP's done

--- Detection/scale/countdirs.pl
+++ Detection/scale/countdirs.pl
@@ -6,10 +6,10 @@ our %flows=();
 our %hash=();
 sub wanted {
     if (-f) {
-	if ($File::Find::name =/nfs_ds/users/mirkovic/nfs_ds /\/ft-v05/)
+	if ($File::Find::name =~ /\/ft-v05/)
 	{
 	    $fname = $File::Find::name;
-	    $fname =/nfs_ds/users/mirkovic/nfs_ds /(.*)(\/ft-v05\.)(.*\.\d\d)(.*)/;
+	    $fname =~ /(.*)(\/ft-v05\.)(.*\.\d\d)(.*)/;
 	    $time = $3;	    
 	    $hash{$time}{$fname} = 1;
 	}
@@ -23,12 +23,24 @@ for $time (sort {$a<=>$b} keys %hash)
     $i = 0;
     $start = 0;
     %fds=();
-    #for $file (keys %{$hash{$time}})
-    #{
-#	system("python reader.py -f flow-tools $file &");
-	print "python reader.py -f flow-tools $file &";
+    my @pids;
+    for $file (keys %{$hash{$time}})
+    {
+	die "could not fork" unless defined(my $pid = fork);
+    	unless ($pid) { #child execs
+        	#exec "python reader.py -f flow-tools $file &";
+		exec "python tempsleep.py";
+        	die "exec failed";
+    	}
+	push @pids, $pid; #parent stores children's pids
+	# system("python reader.py -f flow-tools $file &");
+	print +(split '/', $file )[6], "\n";
 	# wait here for all 29 to finish
-    #}
+    }
+    #wait for all children to finish
+    for my $pid (@pids) {
+    	waitpid $pid, 0;
+    }
 }
 
 sub printStats
