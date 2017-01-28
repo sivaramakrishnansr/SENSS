1245f2f756c857d47b4391599efa69dfd9a0aeae
Steel ISI <ahanamsa@usc.edu>
2017-01-27 16:21:30 -0800

    test

--- Detection/scale/countdirs.pl
+++ Detection/scale/countdirs.pl
@@ -20,27 +20,28 @@ find(\&wanted, $ARGV[0]);
 $d=scalar(keys %hash);
 for $time (sort {$a<=>$b} keys %hash)
 {
-    $i = 0;
-    $start = 0;
-    %fds=();
-    my @pids = ();
-    for $file (keys %{$hash{$time}})
-    {
-	die "could not fork" unless defined(my $pid = fork);
-    	unless ($pid) { #child execs
-        	print "python reader.py -f flow-tools $file\n";
+    #$i = 0;
+	print $time;
+    #$start = 0;
+    #%fds=();
+    #my @pids = ();
+    #for $file (keys %{$hash{$time}})
+    #{
+	#die "could not fork" unless defined(my $pid = fork);
+    	#unless ($pid) { #child execs
+     #   	print "python reader.py -f flow-tools $file\n";
 		#print $file, "\n";
-		exec "python tempsleep.py";
-        	die "exec failed";
-    	}
-	push @pids, $pid; #parent stores children's pids
+	#	exec "python tempsleep.py";
+        #	die "exec failed";
+    	#}
+	#push @pids, $pid; #parent stores children's pids
 	# system("python reader.py -f flow-tools $file &");
 	#print +(split '/', $file )[6], "\n";
-    }
+    #}
     #wait for all children to finish
-    for my $pid (@pids) {
-    	waitpid $pid, 0;
-    }
+    #for my $pid (@pids) {
+    #	waitpid $pid, 0;
+    #}
     #print "$i";
     print "\n-------\n";
     #exit;
