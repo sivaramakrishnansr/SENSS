aa6d1af0990fbb78635a4fde9ca179f3c40c4a60
Ameya <ameyah@live.com>
2017-04-17 12:00:20 -0700

    debug

--- Detection/scale/server.py
+++ Detection/scale/server.py
@@ -102,6 +102,8 @@ class RemoteClient(asyncore.dispatcher):
         if client_message == "file":
             with open("reader_dumps/" + self.name, "r") as reader_json_file:
                 data = json.load(reader_json_file)
+                for single_data in data:
+                    all_data[self.name].append((single_data['time'], single_data['destinations']))
                 self.host.client_message_handle(data, reader_name=self.name)
         elif client_message == "close" or client_message == "":
             closed_clients.append(self.name)
