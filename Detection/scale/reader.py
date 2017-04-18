94173a361ac4d6a344303c6750497322dc043c71
Ameya <ameyah@live.com>
2017-04-18 00:08:03 -0700

    debug

--- Detection/scale/reader.py
+++ Detection/scale/reader.py
@@ -49,7 +49,6 @@ class Client(asyncore.dispatcher):
         self.self_reqs = 0
         self.fh = open("reader_print.txt", "a")
         # self.fh_flow = open("flow_check/" + self.name, "a")
-        self.send(self.name)
         self.current_flows = [self.prepare_flows(size=100)]
         self.say(self.current_flows[0])
         del self.current_flows[0]
