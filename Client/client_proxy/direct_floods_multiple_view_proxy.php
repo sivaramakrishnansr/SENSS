bce8b3bd3b9a29f77201a3578718bd35b790f6c4
Sivaramakrishnan Satyamangalam Ramanathan <satyaman@users.isi.deterlab.net>
2017-11-13 10:01:07 -0800

    client:proxy

---
+++ Client/client_proxy/direct_floods_multiple_view_proxy.php
@@ -0,0 +1,228 @@
+<!--
+#
+# Copyright (C) 2016 University of Southern California.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+-->
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+          <title>SENSS</title>
+          <link rel="stylesheet" href="css/bootstrap.min.css">
+          <script src="css/jquery.min.js"></script>
+          <script src="css/bootstrap.min.js"></script>
+	  <style>
+                .panel { width:300px; margin:auto; padding: 30px;}
+                .panel-offset-senss { margin:auto;}
+		.second-button{
+    			float:right;
+		}
+          </style> 
+</head>
+
+<body>
+        <nav class="navbar navbar-inverse navbar-static-top">
+                <div class="container-fluid">
+                        <div class="navbar-header">
+                                <a class="navbar-brand" href="direct_floods_form.php">SENSS|Proxy</a>
+                        </div>
+                        <div>
+                                <ul class="nav navbar-nav">
+                                        <li><a href="direct_floods_form.php">Direct Floods</a></li>
+                                </ul>
+    				</a>
+                        </div>
+                </div>
+        </nav>
+<body>
+<center>
+
+<img src="demo_proxy.png" width="100%" height="100%">
+</center>
+<br />
+<br />
+<br />
+<?php
+ 		$servername = "localhost";
+                $username = "root";
+                $password = "usc558l";
+                $dbname = "SENSS";
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                }
+                $sql = "SELECT * FROM DIRECT_FLOODS";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+			$proxy=$row["PROXY"];
+
+                        $proxy=$row["PROXY"];
+                        $attack=$row["ATTACK"];
+                        if ($proxy=="1" && $attack=="1"){
+                                echo '<div class="alert alert-info">';
+                                echo '<strong>Info:</strong> At SENSS proxy at ICN';
+                                echo '</div>';
+                                echo '<div class="alert alert-danger">';
+                                $attack_from=$row["ATTACK_FROM"];
+                                echo '<strong>Info:</strong> Attack traffic from '.$attack_from;
+                                echo '</div>';
+                                echo '<div class="alert alert-info">';
+                                echo '<strong>Info:</strong> Request SENSS ISP to add filters below.';
+                                echo '</div>';
+                        }
+                        if ($proxy=="1" && $attack=="0"){
+                                echo '<div class="alert alert-info">';
+                                echo '<strong>Info:</strong> SENSS ISP at ARNEP,EQUINIX and INTERNEXA';
+                                echo '</div>';
+                                echo '<div class="alert alert-info">';
+                                echo '<strong>Info:</strong> Legit traffic from UEM and ENTEL';
+                                echo '</div>';
+                        }
+
+			$result=json_decode($row["RESULT"],true);
+                        echo '<table  class="table table-striped" align="center" style="width: auto;">';
+ 			echo '<tr><th>DPID</th><th>Action</th><th>Stop Action</th></tr>';
+                        foreach ($result as $dpid=>$count){
+        	                                $dpid_of_switch=$count["DPID"];
+						$isp_of_switch=$count["ISP"];
+						$filter=explode(",",$row["FILTER"]);
+						$remove_action=False;
+						foreach ($filter as $filter_dpid){
+							if($filter_dpid==$dpid_of_switch){
+								$remove_action=True;
+							}
+						}
+
+                                	        if ($count["Mark"] == "True"){
+						   if ($remove_action==True){
+						   echo '<tr><td>'.$isp_of_switch.'</td><td></td><td><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#Remove-'.$dpid.'">Remove Filter</button></td></tr>';
+						   echo '<div id="Remove-'.$dpid.'" class="modal fade" role="dialog">';
+						   echo '<div class="modal-dialog">';
+							 	echo '<div class="modal-content">';
+						 			echo '<div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button>';
+                                                				echo '<h4 class="modal-title">Remove Filter</h4>';
+                                                 			echo '</div>';
+						 			echo '<div class="modal-body">';
+									        echo '<form name="remove_filter_form" id="remove_filter_form" action="remove_filter.php" method="post">';
+										echo '<h3> Are you sure you want to remove this filter?</h3>';
+										echo '<input type="hidden" name="request_id" value="'.$row["ID"].'"/>';
+										echo '<input type="hidden" name="dpid" value="'.$dpid_of_switch.'"/>';
+
+                                                 				echo '<input type="submit" class="btn" name="formSubmit" value="Submit"/> ';
+                                                 				echo '</form> ';
+									echo '</div>';
+								echo '</div>';
+							echo '</div>';
+						echo '</div>';
+
+						   }else{
+
+						   echo '<tr><td>'.$isp_of_switch.'</td>'.'<td><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#Add-'.$dpid.'">Add Filter</button></td><td></td></tr>';
+						   }
+						   echo '<div id="Add-'.$dpid.'" class="modal fade" role="dialog">';
+
+						   echo '<div class="modal-dialog">';
+							 	echo '<div class="modal-content">';
+						 			echo '<div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button>';
+                                                				echo '<h4 class="modal-title">Add Filter</h4>';
+                                                 			echo '</div>';
+						 			echo '<div class="modal-body">';
+									        echo '<form name="add_filter_form" id="add_filter_form" action="add_filter_proxy.php" method="post">';
+
+
+										echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="dpid" id="dpid" maxlength="50"  readonly="readonly" value="'.$dpid_of_switch.'" />';
+                                                				echo '</div>';
+
+										echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="nw_src" id="nw_src" maxlength="50"  placeholder="Source IP" />';
+                                                				echo '</div>';
+                                                				echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="nw_dst" id="nw_dst" maxlength="50"  placeholder="Destination IP" />';
+                                                				echo '</div>';
+                                                				echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="in_port" id="in_port" maxlength="50"  placeholder="In Port" />';
+                                                				echo '</div>';
+                                                				echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="eth_dst" id="eth_dst" maxlength="50"  placeholder="Destination MAC" />';
+                                                				echo '</div>';
+										echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="eth_src" id="eth_src" maxlength="50"  placeholder="Source MAC" />';
+                                                				echo '</div>';
+                                                				echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="dl_vlan" id="dl_vlan" maxlength="50"  placeholder="VLAN ID" />';
+                                                				echo '</div>';
+                                                				echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="tcp_src" id="tcp_src" maxlength="50"  placeholder="TCP Source Port" />';
+                                                				echo '</div>';
+                                                				echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="tcp_dst" id="tcp_dst" maxlength="50"  placeholder="TCP Destination Port" />';
+                                                				echo '</div>';
+										echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="udp_src" id="udp_src" maxlength="50"  placeholder="UDP Source Port" />';
+                                                				echo '</div>';
+                                                				echo '<div class="form-group">';
+                                                					echo '<input type="text" style="width:200px;" class="form-control" name="udp_dst" id="udp_dst" maxlength="50"  placeholder="UDP Destination Port" />';
+                                                				echo '</div>';
+
+
+										echo '<input type="hidden" name="request_id" value="'.$row["ID"].'"/>';
+
+                                                 				echo '<input type="submit" class="btn" name="formSubmit" value="Submit"/> ';
+                                                 				echo '</form> ';
+									echo '</div>';
+								echo '</div>';
+							echo '</div>';
+						echo '</div>';
+						}
+						else{
+						   if ($remove_action==True){
+						   	//echo '<tr><td>'.$dpid.'</td><td>'.$dpid_of_switch.'</td><td>'.$count["Packet Count"][$i].'</td><td>'.$count["Byte Count"][$i].'</td><td>'.$packet_diff.'</td><td>'.$byte_diff.'</td><td></td><td><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#Remove-'.$dpid.'">Remove Filter</button></td></tr>';
+						   echo '<tr><td>'.$isp_of_switch.'</td><td></td><td><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#Remove-'.$dpid.'">Remove Filter</button></td></tr>';
+						   echo '<div id="Remove-'.$dpid.'" class="modal fade" role="dialog">';
+						   echo '<div class="modal-dialog">';
+							 	echo '<div class="modal-content">';
+						 			echo '<div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button>';
+                                                				echo '<h4 class="modal-title">Add Filter</h4>';
+                                                 			echo '</div>';
+						 			echo '<div class="modal-body">';
+									        echo '<form name="remove_filter_form" id="remove_filter_form" action="remove_filter_proxy.php" method="post">';
+										echo '<h3> Are you sure you want to remove this filter?</h3>';
+
+										echo '<input type="hidden" name="request_id" value="'.$row["ID"].'"/>';
+										echo '<input type="hidden" name="dpid" value="'.$dpid_of_switch.'"/>';
+
+                                                 				echo '<input type="submit" class="btn" name="formSubmit" value="Submit"/> ';
+                                                 				echo '</form> ';
+									echo '</div>';
+								echo '</div>';
+							echo '</div>';
+						echo '</div>';
+
+						   }else{
+						   	//echo '<tr><td>'.$dpid.'</td><td>'.$dpid_of_switch.'</td><td>'.$count["Packet Count"][$i].'</td><td>'.$count["Byte Count"][$i].'</td><td>'.$packet_diff.'</td><td>'.$byte_diff.'</td></tr>';
+						   	echo '<tr><td>'.$isp_of_switch.'</td><td></td><td></td></tr>';
+						   }
+						}
+						//echo '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#123">Add Filter</button>';
+				}
+		}
+	}
+
+?>
+</body>
+</html>
---
+++ Client/client_proxy/remove_filter_proxy.php
@@ -0,0 +1,164 @@
+<!--
+#
+# Copyright (C) 2016 University of Southern California.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+-->
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+          <title>My Form</title>
+          <link rel="stylesheet" href="css/bootstrap.min.css">
+          <script src="css/jquery.min.js"></script>
+          <script src="css/bootstrap.min.js"></script>
+	  <style>
+                .panel { width:300px; margin:auto; padding: 30px;}
+                .panel-offset-senss { margin:auto;}
+          </style> 
+</head>
+
+<body>
+        <nav class="navbar navbar-inverse navbar-static-top">
+                <div class="container-fluid">
+                        <div class="navbar-header">
+                                <a class="navbar-brand" href="index.php">SENSS</a>
+                        </div>
+                        <div>
+                                <ul class="nav navbar-nav">
+                                        <li><a href="add_filter_form.php">Add Filter</a></li>
+                                        <li><a href="get_flows_form.php">Flow Stats</a></li>
+                                        <li><a href="get_routes_form.php">Route Stats</a></li>
+                                        <li><a href="pic.php">View</a></li>
+					<li><a href="direct_floods_form.php">Direct Floods</a></li>
+                                </ul>
+                        </div>
+                </div>
+        </nav>
+ <form>
+
+<table>
+ <?php
+        if($_POST['formSubmit'] == "Submit")
+        {
+		$match_list=array();
+		foreach ($_POST as $key => $value) {
+			if($key=="dpid"){
+				$dpid=$value;
+				continue;
+			}
+			if(strlen($value)!=0 && $value!=Submit){
+
+				$match_list[$key]=$value;
+				if($key=="tcp_src" || $key=="tcp_dst"){
+					$match_list["ip_proto"]=6;
+				}
+				if($key=="udp_src" || $key=="udp_dst"){
+					$match_list["ip_proto"]=17;
+				}
+
+			}
+		}
+
+
+                $data_to_send=array();
+                $data_to_send["dpid"]=$dpid;
+                $data_to_send["priority"]=0;
+
+                $match_list["in_port"]=2;
+                $data_to_send["match"]=$match_list;
+		$temp_array=array();
+		$temp_array["type"]="OUTPUT";
+		$temp_array["port"]=1;
+                $data_to_send["actions"]=array($temp_array);
+		print_r($data_to_send);
+                $ip_filter = $_POST['ip_filter'];
+                $dpid = $_POST['dpid'];
+                $data_string = json_encode($data_to_send);
+                $url='http://localhost:8080/stats/flowentry/add';
+                $ch=curl_init($url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                        'Content-Type: application/json',
+                        'Content-Length: ' . strlen($data_string))
+                );
+                $result = curl_exec($ch);
+                curl_close($ch);
+                $data_to_send=array();
+                $data_to_send["dpid"]=$dpid;
+                $data_to_send["priority"]=0;
+
+                $match_list["in_port"]=1;
+                $data_to_send["match"]=$match_list;
+		$temp_array=array();
+		$temp_array["type"]="OUTPUT";
+		$temp_array["port"]=2;
+                $data_to_send["actions"]=array($temp_array);
+		print_r($data_to_send);
+                $ip_filter = $_POST['ip_filter'];
+                $dpid = $_POST['dpid'];
+                $data_string = json_encode($data_to_send);
+                $url='http://localhost:8080/stats/flowentry/add';
+                $ch=curl_init($url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                        'Content-Type: application/json',
+                        'Content-Length: ' . strlen($data_string))
+                );
+                $result = curl_exec($ch);
+                curl_close($ch);
+
+                $json_output = json_decode($result,true);
+		$results_to_store=$dpid;
+                $servername = "localhost";
+                $username = "root";
+                $password = "usc558l";
+                $dbname = "SENSS";
+                $conn = new mysqli($servername, $username, $password, $dbname);
+		$request_id=$_POST["request_id"];
+                if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                }
+		$sql ="SELECT * FROM DIRECT_FLOODS WHERE ID='$request_id'";
+                $result = $conn->query($sql);
+		 while($row = $result->fetch_assoc()) {
+			if (strlen($row["FILTER"])!=0){
+				$filter=$row["FILTER"];
+			}
+			else{
+				$filter="";
+			}
+		}
+		$filter=explode(",",$filter);
+		$filter=array_diff($filter,array($dpid));
+		$filter=implode(",",$filter);
+		$sql="UPDATE DIRECT_FLOODS SET FILTER='$filter' WHERE ID='$request_id'";
+		echo $sql;
+                $result = $conn->query($sql);
+		echo '<br />';
+		echo '<h1>Filter Removed-'.$dpid.'</h1>';
+
+                header("Location: http://localhost:8181/direct_floods_multiple_view_proxy.php"); 
+                exit();
+        }
+  ?>
+</table>
+
+</body>
+</html>
